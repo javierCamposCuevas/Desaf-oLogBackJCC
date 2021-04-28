70248179c18cb003b2b1fd93064bed22e8fec80d
JavierCampos <jcampocu@host.docker.internal>
2021-04-28 17:03:46 +0200

    Proyecto arreglado

---
+++ everis-logback-1jcc/src/main/java/com/everis/logback/App.java
@@ -0,0 +1,52 @@
+package com.everis.logback;
+
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	static Scanner sc = new Scanner(System.in);
+	final static Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void equipo(int equipo) {
+		LOGGER.info("Iniciando método equipo");
+		switch (equipo) {
+		case 1:
+			LOGGER.info("Sevilla: Muy buena elección");
+			System.out.println("Bien bien así se hace");
+			break;
+		case 2:
+			LOGGER.info("Betis: Amante de las vitrinas vacías por lo que veo");
+			System.out.println("Buen material para meme");
+			break;
+		case 3:
+			LOGGER.info("Madrird: Con los pedazo de equipos uqe hay aquí y escoges esta cosa");
+			System.out.println("Real Mandril, digo Madrid");
+			break;
+		case 4:
+			LOGGER.error("Barça: Esto es un error");
+			System.err.println("Sigue siendo un error");
+			break;
+		case 0:
+			LOGGER.info("Sigo pensando que me merezco un 10");
+			System.out.println("Creo que me merezco un 10 Tellez");
+			LOGGER.debug("Finalizando el método equipo");
+			break;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Elija una opción:");
+		System.out.println("1.Sevilla\n" + "2.Betis\n" + "3.Real Madrid\n" + "4.Barça\n" + "0.Salir\n");
+		int opt = sc.nextInt();
+		equipo(opt);
+
+	}
+}
